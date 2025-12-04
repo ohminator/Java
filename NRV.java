557c94e4345b471ea620b6151ff4bb76f7e0e5a3
ohminator <schwimmer.elegant6c@icloud.com>
2025-12-04 12:16:15 +0100

    Update NRV.java

--- NRV.java
+++ NRV.java
@@ -16,12 +16,47 @@ public class NebenRollenVermittlung {
             }
             setcards.next();
         }
+        return null;
     }
 
     public Nebenrolle sucheEigenschaft(String pAttribut, String pEigenschaft) {
         setcards.toFirst();
         while(setcards.hasAccess()) {
-            
+            if(pAttribut == "Alter") {
+                if(pEigenschaft.equals(setcards.getContent().getAlter())) {
+                    return setcards.getContent();
+                }
+                setcards.next();
+            }
+            if(pAttribut == "Geschlecht") {
+                if(pEigenschaft.equals(setcards.getContent().getGeschlecht())) {
+                    return setcards.getContent();
+                }
+                setcards.next();
+            }
+            if(pAttribut == "Größe") {
+                if(pEigenschaft.equals(setcards.getContent().getGröße())) {
+                    return setcards.getContent();
+                }
+                setcards.next();
+            }
         }
+        return null;
+    }
+
+    public void nebenrolleHinzufügen(Nebenrolle pNeueNebenrolle) {
+        new Nebenrolle pNeueNebenrolle;
+        setcards.append(pNeueNebenrolle);
+    }
+
+    public void nebenrolleLöschen() {
+        setcards.remove();
+    }
+
+    public Nebenrolle aktNeberolle() {
+        while(setcards.hasAccess()) {
+            return setcards.getContent();
+        }
+        return null;
     }
 }
